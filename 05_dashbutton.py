7b5f4bdd645e32c69ab3f363c05664bb7418cdaf
ecg1 <ecg1@discard.email>
2017-01-20 10:04:54 +0000

    Datei umbenannt

--- 05_dashbutton.py
+++ 05_dashbutton.py
@@ -1,48 +1,31 @@
-import datetime
-import logging
-import urllib2
- 
-# Constants
-timespan_threshhold = 3
- 
-# Globals
-lastpress = datetime.datetime(1970,1,1)
- 
-logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
-from scapy.all import *
- 
- 
-def button_pressed_dash1():
-  print "Button1 init..."
-  global lastpress
-  thistime = datetime.datetime.now()
-  timespan = thistime - lastpress
-  if timespan.total_seconds() > timespan_threshhold:
-    current_time = datetime.datetime.strftime(thistime, '%Y-%m-%d %H:%M:%S')
-    print 'Dash button pressed at ' + current_time
-#    urllib2.urlopen('http://pi:8083/fhem?cmd.LED=set%20LED%20toggle&room=Haussteuerung')
- 
-  lastpress = thistime
- 
-def button_pressed_dash2():
-  print "Button2 init..." 
+import logging # for the following line
 
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR) # suppress IPV6 warning on startup
 
-def udp_filter(pkt):
-  options = pkt[DHCP].options
-  for option in options:
-    if isinstance(option, tuple):
-      if 'requested_addr' in option:
-        # we've found the IP address, which means its the second and final UDP request, so we can trigger our action
-        mac_to_action[pkt.src]()
-        break
+from scapy.all import * # for sniffing for the ARP packets
+import requests # for posting to the IFTTT Maker Channel
  
+# it takes a minute for the scapy sniffing to initialize, so I print this to know when it's actually ready to go
+print('Init Amazon Dash Button sniffer... done.')
  
-mac_to_action = {'ac:63:be:6c:28:af' : button_pressed_dash1, 'AC:63:BE:6C:28:AF' : button_pressed_dash2 }
-mac_id_list = list(mac_to_action.keys())
+def button1_pressed(mac) :
+  print('')
+  print('######################################################')
+  print('### Amazon Dash Button pressed (' + mac + ') ###') 
+  print('######################################################')
+
+def arp_display(pkt):
+  mac_actions = { 'ac:63:be:c6:28:af' : button1_pressed }
+  mac_list = list(mac_actions.keys())
+
+#  print('------------------')
+#  print('method init...')
+#  print('hw.src=' + pkt[ARP].hwsrc)
+#  print('hw.psrc=' + pkt[ARP].psrc)
+  if pkt[ARP].op == 1: #who-has (request)
+      if pkt[ARP].hwsrc in mac_list : # this is the first button MAC address
+        mac_actions[pkt[ARP].hwsrc](pkt[ARP].hwsrc)
+#      else:
+#        print("ARP Probe from unknown device: " + pkt[ARP].hwsrc)
  
-print "Waiting for a button press..."
-sniff(prn=udp_filter, store=0, filter="udp", lfilter=lambda d: d.src in mac_id_list)
- 
-if __name__ == "__main__":
-  main()
+print(sniff(prn=arp_display, filter="arp", store=0))
