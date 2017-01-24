a267e8dff3ea61315a9803258fd66a755cced249
ecg1 <ecg1@discard.email>
2017-01-24 06:12:04 +0000

    auto commit by ecg1

--- 07_system.sh
+++ 07_system.sh
@@ -1,30 +1,49 @@
 #!/bin/bash
 
+# Waittime to redo messurement
+SLEEP_LOOP=30
+
+# #################################################
+
+DISPLAY_ECHO=0
+while [[$# -gt 1]]
+	do
+	key="$1"
+
+	case $key in
+	-display)
+		DISPLAY_ECHO=1
+	;;
+	*)
+	;;
+esac
+
+
 while true
 do
         CPU=`top -d 0.5 -b -n2 | grep "Cpu(s)"|tail -n 1 | awk '{print $2 + $4}'`
-        echo "cpu: $CPU"
+        [[ DISPLAY_ECHO == 1 ]] && echo "cpu: $CPU"
 	# Freier Speicher
         MEM_FREE=`free -m | grep "Mem:" | awk '{print $4}'`
-	echo "mem free: $MEM_FREE"
+	[[ $DISPLAY_ECHO == 1 ]] && echo "mem free: $MEM_FREE"
 	# netload eth0 in
 	ETH0_IN=`ifstat -i eth0 -q 1 1 | sed -n '3p' | cut -d' ' -f 5`
-	echo "eth0 in: $ETH0_IN"
+	[[ $DISPLAY_ECHO == 1 ]] && echo "eth0 in: $ETH0_IN"
 	# netload eth0 outt
 	ETH0_OUT=`ifstat -i eth0 -q 1 1 | sed -n '3p' | cut -d' ' -f 11`
-	echo "eth0 out: $ETH0_OUT"
+	[[ $DISPLAY_ECHO == 1 ]] && echo "eth0 out: $ETH0_OUT"
 	# netload wlan in
 	WLAN0_IN=`ifstat -i wlan0 -q 1 1 | sed -n '3p' | cut -d' ' -f 5`
-	echo "wlan in: $WLAN0_IN"
+	[[ $DISPLAY_ECHO == 1 ]] && echo "wlan in: $WLAN0_IN"
 	# netload wlan out
 	WLAN0_OUT=`ifstat -i wlan0 -q 1 1 | sed -n '3p' | cut -d' ' -f 5`
-	echo "wlan0 out: $WLAN0_OUT"
+	[[ $DISPLAY_ECHO == 1 ]] && echo "wlan0 out: $WLAN0_OUT"
 	# rpi voltage
 	RPI_VOLTAGE=`vcgencmd measure_volts core | cut -d'=' -f 2 | cut -d'V' -f 1`
-	echo "voltage: $RPI_VOLTAGE"
+	[[ $DISPLAY_ECHO ==1 ]] && echo "voltage: $RPI_VOLTAGE"
 	# sdcard_free (%)
 	SDCARD_FREE=`df -h | grep mmcb | awk '{print $5}' | cut -d'%' -f 1`
-
+	[[ $DISPLAY_ECHO == 1 ]] && echo "sd free: SDCARD_FREE"
 
 	URL=`cat /home/pi/circonus/rpi_ecg1_url.txt`
 
@@ -38,6 +57,6 @@ do
 	    "RPI.sdcard_free": "'$SDCARD_FREE'",
 	    "RPI.voltage": "'$RPI_VOLTAGE'"
           }'
-        sleep 30
+        sleep $SLEEP_LOOP
 done
 
